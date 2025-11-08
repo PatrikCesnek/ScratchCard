8d9f9720c9fec73f95e64dc152bb326978978308
Patrik Cesnek <pcesnek290@gmail.com>
2025-11-08 14:52:35 +0100

    Initial Commit

---
+++ ScratchCard/ScratchCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScratchCardApp.swift
+//  ScratchCard
+//
+//  Created by Patrik Cesnek on 08/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScratchCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScratchCardTests/ScratchCardTests.swift
@@ -0,0 +1,16 @@
+//
+//  ScratchCardTests.swift
+//  ScratchCardTests
+//
+//  Created by Patrik Cesnek on 08/11/2025.
+//
+
+import Testing
+
+struct ScratchCardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
